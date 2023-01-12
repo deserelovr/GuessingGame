814bcd52ca081637d0f68563db3f101925fcbcc2
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-01-12 11:26:44 -0800

    added more fun lang.

--- GuessingGame.py
+++ GuessingGame.py
@@ -10,18 +10,20 @@ def display_title():
 
 def play_game(LIMIT):    
     number = random.randint(1, LIMIT)
+    print("Lets play a guessing game!")
     print(f"I'm thinking of a number from 1 to {LIMIT}\n")
     count = 1
     guess = int(input("Your guess: "))
 
     while (guess != number):
         if guess < number:
-            print("Too low. Try Again. ")
+            print("Oh no! Too low. Try Again. ")
             count += 1
         elif guess > number:
-            print("Too high. Try Again. ")
+            print("Oh no! Too high. Try Again. ")
             count += 1
         guess = int(input("Your guess: "))
+        print("Woohoo! You got it!")
     print(f"You guessed it in {count} tries.\n")
      
 def main():
@@ -32,7 +34,7 @@ def main():
         play_game(LIMIT)
         again = input("Would you like to play again? Please enter 'y' for yes or 'n' for no: ")
         print()
-    print("See Ya!")
+    print("See Ya Later!")
 
 if __name__ == "__main__":
     main()
